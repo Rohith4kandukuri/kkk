976936590f9918af9efa01ea225bd2d0f2e87714
Rohith  Kandukuri <118825627+Rohith4kandukuri@users.noreply.github.com>
2024-10-21 16:45:23 +0530

    Add files via upload

---
+++ Spamdetector.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+
+model=pickle.load(open("spam.pkl","rb"))
+cv=pickle.load(open("vectorizer.pkl","rb"))
+
+
+def main():
+	st.title("Email Spam Claasification Apps")
+	st.subheader("Bulid With streamlit & Python")
+	msg=st.text_input("Enter a Text:")
+	if st.button("Predict"):
+		data=[msg]
+		vect=cv.transform(data).toarray()
+		prediction=model.predict(vect)
+		result=prediction[0]
+		if result==1:
+			st.error("This is a spam mail")
+		else:
+			st.success("This is a ham mail")
+
+main()
